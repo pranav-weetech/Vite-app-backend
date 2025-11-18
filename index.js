631a6ea22dd1927beb86d854c0e9dfcae466c85b
pranav-weetech <pranav@weetechsolution.com>
2025-11-18 09:44:33 +0530

    backend

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(
 // 📦 Routes
 // ===============================================
 app.get("/", (req, res) => {
-  res.send("Backend is running ✅");
+  res.send("BACKENDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD✅");
 });
 
 app.use("/api/todo", todoRoutes);
