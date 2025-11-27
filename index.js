4af3e22ba854663f578cba9041c35f1671af84ba
pranav-weetech <pranav@weetechsolution.com>
2025-11-27 13:06:22 +0530

    bck

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(
 // 📦 Routes
 // ===============================================
 app.get("/", (req, res) => {
-  res.send("BACKEND ✅");
+  res.send("Bcknd✅");
 });
 
 app.use("/api/todo", todoRoutes);
